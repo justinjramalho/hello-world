9a6be14a130a92c90f6dfe1e76bd5a2a058e5983
justinjramalho <45409057+justinjramalho@users.noreply.github.com>
2019-12-02 12:19:43 -0500

    Hello-user JJR_Update
    JJR
    12-02-19
    12:18pm

---
+++ hello-user.py
@@ -0,0 +1,14 @@
+#
+# Hello World – User Input
+#
+# Author: Justin J Ramalho
+#
+# Purpose: 
+#
+# hello-user.py
+#
+
+
+name = input ("Who would you like to say 'Hello' to?!  Enter their name here: ")
+print ("Hello", name)
+
