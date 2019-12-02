e463ce3e44ae192d9cc867a8618232fc6073c30f
justinjramalho <45409057+justinjramalho@users.noreply.github.com>
2019-12-02 12:00:17 -0500

    Uploaded Python file – "Hello"
    uploaded Python file – "Hello"
    
    JJR
    12-02-19
    12:00pm

---
+++ hello.py
@@ -0,0 +1,30 @@
+#
+# This is a comment 
+#
+# PMDS Unit 1 – My first Python Program 
+#
+# Author: Justin J Ramalho
+#
+# Purpose: A classing introduction to programming in Python – "Hello World"
+#
+# hello.py
+#
+
+
+print("Hello World")
+
+'''
+
+Anything typed after a hashtag and/or three quotation
+will be ignored by the Python Interpreter
+(not exactly 'ignored')
+but it is incredibly important for organized
+and clear comments, when working with a team
+for programming – think bigger scope – think documentation,
+like in medicine, or teaching (test grades, misbehavior, etc.)
+
+'''
+
+print ("Done with my comments... on #comments.")
+
+    
